3d6e516d2f8f1c559403d2734b92ca55a37f7495
Briscoe Fletcher <52837237+Fletch1881@users.noreply.github.com>
2021-03-01 21:12:18 -0600

    Testing

---
+++ Hello.java
@@ -0,0 +1,23 @@
+public class Hello{
+	
+public static void main(String ar[]){
+System.out.print("Hello World...");
+
+}
+}
+
+/*
+access specifiers or visibility modes:
+	public - visible throughout the app
+	protected - visible wihtin the packages and any of the child classes which is outside the package
+	default - package level scope, only visible within the package
+	private - visible only within the class
+*/
+
+//javac -d . HeyAgain   creates the package(or new directory) for the class
+//javac   is for compiling
+//java  is for running 
+
+//git add .	//adds documents to push
+// git commit -m   //commits it with notes
+// git push	// pushes the commit
